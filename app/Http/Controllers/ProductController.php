90e11a0bee79670fd22a8a96e0641d667b6c4dd7
Aramdhann <radityamochamad@gmail.com>
2024-03-14 14:03:24 +0700

    make controller create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,15 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Validation\Validator;
 
 class ProductController extends Controller
 {
     public function index(): View
     {
-        $product = Product::query()->latest()->paginate(10);
+        $products = Product::query()->latest()->paginate(10);
 
-        return view('products.index', compact('product'));
+        return view('products.index', compact('products'));
+    }
+
+    public function create(): View
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg, jpg, png|max:2048',
+            'title' => 'required|min:5',
+            'description' => 'required|min:10',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+
+        $image = $request->file('image');
+        $image->storeAS('public/products', $image->hashName());
+
+        Product::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock,
+        ]);
+
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 }
